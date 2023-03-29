b33711f2fe3cb74cefb57575b2fb92ac35cecb38
hyosin <>
2023-03-30 07:52:38 +0900

    style: styled.d.ts, index.css 추가

---
+++ src/styled.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    color: {
+      black: '#000000';
+      black2: '#151515';
+      white: '#ffffff';
+      point: '#ff366a';
+      grey: '#5b5b5b';
+      grey100: '#767676';
+      grey200: '#f5f5f5';
+    };
+  }
+}

2fcc0308e9087b3eb30450f9f8e5462403d1e57c
hyosin <>
2023-03-30 07:41:59 +0900

    feat: 채팅 내역 쌓이게 구현

--- src/pages/messenger.tsx
+++ src/pages/messenger.tsx
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 import useInput from '../hooks/useInput';
 
 const Messenger = () => {
-  // 현재 유저 아이디
-  const [curUserId, setCurUserId] = useState<number>(1); // userId 저장
+  // 내 유저 아이디
+  const [curUserId, setCurUserId] = useState<number>(1);
+
+  // 상대 유저 아이디
+  const [otherUserId, setOtherUserId] = useState<number>(2);
 
   // 채팅 내역 창고
   const [messageStorage, setMessageStorage] = useState([
@@ -40,16 +43,16 @@ const Messenger = () => {
   return (
     <div>
       <h1>채팅</h1>
-      <div>
+      <MessageContainer>
         {/* 채팅 내역 쌓이는 컴포넌트 */}
         {messageStorage &&
           messageStorage.map((message, idx) => (
-            <div key={idx}>
+            <Message key={idx} curUser={message.userId === curUserId}>
               <span>{message.userId}</span> <span>{message.data}</span>{' '}
               <span>{message.timestamp}</span>
-            </div>
+            </Message>
           ))}
-      </div>
+      </MessageContainer>
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={input.onChange} />
         <button type="submit">전송</button>
@@ -59,3 +62,14 @@ const Messenger = () => {
 };
 
 export default Messenger;
+
+const MessageContainer = styled.div`
+  display: flex;
+  width: 30rem;
+  height: 40rem;
+  background-color: pink;
+  flex-direction: column;
+  overflow-y: auto;
+`;
+
+const Message = styled.div<{ curUser: boolean }>``;

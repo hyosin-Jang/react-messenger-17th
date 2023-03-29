98960109e1f167b9a222dbdbbbeb4aac1fe41d9f
hyosin <>
2023-03-30 08:59:49 +0900

    feat: 버튼 클릭 시 유저 정보, 채팅내역 json으로 저장

--- src/pages/messenger.tsx
+++ src/pages/messenger.tsx
@@ -1,6 +1,9 @@
 import { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import useInput from '../hooks/useInput';
+import { useRecoilValue } from 'recoil';
+import { userGroup } from '../utils/atom';
+import { UserInfoType } from '../utils/type';
 
 const Messenger = () => {
   // 내 유저 아이디
@@ -19,6 +22,9 @@ const Messenger = () => {
   ]);
 
   const input = useInput('');
+
+  const curUserGroup = useRecoilValue(userGroup);
+  console.log('curUserGroup', curUserGroup);
 
   // 내 아이디 <-> 상대 아이디
   const handleUserToggle = (e: any) => {
@@ -54,12 +60,34 @@ const Messenger = () => {
       timestamp: new Date().toString(),
     };
 
-    console.log('newMessage', newMessage);
+    // console.log('newMessage', newMessage);
 
     setMessageStorage([...messageStorage, newMessage]);
     // input 초기화
     input.setValue('');
     e.target.reset();
+  };
+
+  // 유저 정보랑 채팅 내역 json으로 저장하는 함수
+  const saveToJson = (e: any) => {
+    e.preventDefault();
+
+    const userState = curUserGroup.find((item) => item.userId === curUserId);
+    const otherUserState = curUserGroup.find(
+      (item) => item.userId === otherUserId
+    );
+    // console.log('userState', userState, otherUserState);
+
+    const combine = [userState, otherUserState, ...messageStorage];
+
+    const jsonCombine = JSON.stringify(combine);
+    const blob = new Blob([jsonCombine], { type: 'application/json' });
+
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'chatting_info.json';
+    link.click();
   };
   return (
     <div>
@@ -81,6 +109,9 @@ const Messenger = () => {
         <input type="text" onChange={input.onChange} autoFocus />
         <button type="submit">전송</button>
       </form>
+      <button type="button" onClick={saveToJson}>
+        json으로 저장
+      </button>
     </div>
   );
 };

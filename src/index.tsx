ca6e65fbfe0b4391855a468099e1d0382c72c3d5
hyosin <>
2023-03-30 17:15:25 +0900

    style: 전역 스타일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import { GlobalStyle } from 'styles/globalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <RecoilRoot>
+    <GlobalStyle />
     <App />
   </RecoilRoot>
 );

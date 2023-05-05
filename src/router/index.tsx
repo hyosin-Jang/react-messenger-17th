ed762351fefcf0beb0763c9a2402df94319366e9
hyosin <>
2023-05-05 21:36:51 +0900

    chore: 유저 플로우대로 코드 순서 변경

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Friends from '../pages/friends';
-import ChatRooms from '../pages/chatRooms';
-import Messenger from '../pages/messenger';
-import OnBoarding from '../pages/onBoarding';
+import Friends from 'pages/friends';
+import ChatRooms from 'pages/chatRooms';
+import Messenger from 'pages/messenger';
+import OnBoarding from 'pages/onBoarding';
+import Settings from 'pages/settings';
 
 const MessengerRouter = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<OnBoarding />} />
+        <Route path="/friends" element={<Friends />} />
         <Route path="/chats" element={<ChatRooms />} />
         <Route path="/chats/:roomId" element={<Messenger />} />
-        <Route path="/friends" element={<Friends />} />
-        <Route path="/" element={<OnBoarding />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );

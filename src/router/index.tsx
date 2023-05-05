9516854f63b3e825830d13372a3ebe8fde845158
hyosin <>
2023-05-05 20:29:08 +0900

    feat: 온보딩, 채팅방, 채팅 리스트, 친구 목록 페이지 라우팅 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Friends from '../pages/friends';
+import ChatRooms from '../pages/chatRooms';
 import Messenger from '../pages/messenger';
+import OnBoarding from '../pages/onBoarding';
 
 const MessengerRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Messenger />} />
+        <Route path="/chats" element={<ChatRooms />} />
+        <Route path="/chats/:roomId" element={<Messenger />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/" element={<OnBoarding />} />
       </Routes>
     </Router>
   );

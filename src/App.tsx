b29505fce8b42f5f574b245f53ae8e6893266ee3
hyosin <>
2023-05-07 06:52:41 +0900

    fix: 기본 유저 atom 초기값 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,6 @@
 import MessengerRouter from './router';
-import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { userGroup } from './utils/atom';
-import { UserInfoType } from './utils/type';
 
 function App() {
-  const setUsers = useSetRecoilState(userGroup);
-
-  const nameArr = ['hani', 'herin', 'hyein', 'minij'];
-  const testUserGroup: UserInfoType[] = Array.from(
-    { length: nameArr.length },
-    (_, i) => ({
-      userId: i + 1,
-      name: nameArr[i],
-      statusMessage: '호호',
-    })
-  );
-
-  const setDefaultUser = () => {
-    const me: UserInfoType = {
-      userId: 0,
-      name: 'hyosin',
-      statusMessage: '하하',
-    };
-
-    // 기본 유저 아이디 설정
-    setUsers((prev) => [...prev, ...testUserGroup, me]);
-  };
-
-  useEffect(() => {
-    setDefaultUser();
-  }, []);
-
   return (
     <>
       <MessengerRouter />

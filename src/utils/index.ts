f6c29367e94bdcd4b30c3f834ace1fd11809ee5a
hyosin <>
2023-05-07 15:27:43 +0900

    chore: setSearchFilter 유틸 함수로 분리

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,15 @@
+import { UserInfoType } from './type';
+
 export const setDateFormat = (date: string) => {
   const hours = new Date(date).getHours().toString().padStart(2, '0');
   const minutes = new Date(date).getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes}`;
+};
+
+export const setSearchFilter = (searchTerm: string, user: UserInfoType) => {
+  if (searchTerm === '') {
+    return user;
+  } else if (user.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+    return user;
+  }
 };

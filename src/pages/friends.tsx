ca7c3bbb92948ad8e4528acf95c3c6cdde530508
hyosin <>
2023-05-07 16:02:13 +0900

    fix: bgShape 스타일 theme으로 분리

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -3,7 +3,7 @@ import Header from 'components/Header';
 import NavBar from 'components/NavBar';
 
 import styled from 'styled-components';
-import { flexCenter } from 'styles/theme';
+import { backgroundShape, flexCenter } from 'styles/theme';
 import Search from 'assets/icon-search.png';
 
 import useInput from 'hooks/useInput';
@@ -124,23 +124,7 @@ const InputWrapper = styled.div`
 
 const Wrapper = styled.main`
   ${flexCenter}
-  flex-direction: column;
-  width: 30rem;
-  height: 50rem;
-  border-radius: 2rem;
-  background: radial-gradient(
-      178.94% 106.41% at 26.42% 106.41%,
-      #fff7b1 0%,
-      rgba(255, 255, 255, 0) 71.88%
-    )
-    #ffffff;
-
-  box-shadow: 0px 155px 62px rgba(0, 0, 0, 0.01),
-    0px 87px 52px rgba(0, 0, 0, 0.05), 0px 39px 39px rgba(0, 0, 0, 0.09),
-    0px 10px 21px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);
-
-  border-radius: 23px;
-  transition: all 0.8s cubic-bezier(0.15, 0.83, 0.66, 1);
+  ${backgroundShape}
 `;
 
 const ProfileImg = styled.img`
--- src/pages/messenger.tsx
+++ src/pages/messenger.tsx
@@ -19,7 +19,7 @@ import { ChatMessageType, ChatRoomInfoType } from 'utils/type';
 
 // style
 import styled from 'styled-components';
-import { flexCenter } from 'styles/theme';
+import { backgroundShape, flexCenter } from 'styles/theme';
 import Send from 'assets/icon-send.png';
 import Save from 'assets/icon-save.png';
 import { theme } from 'styles/theme';
@@ -165,23 +165,7 @@ export default Messenger;
 
 const Wrapper = styled.main`
   ${flexCenter}
-  flex-direction: column;
-  width: 30rem;
-  height: 50rem;
-  border-radius: 2rem;
-  background: radial-gradient(
-      178.94% 106.41% at 26.42% 106.41%,
-      #fff7b1 0%,
-      rgba(255, 255, 255, 0) 71.88%
-    )
-    #ffffff;
-
-  box-shadow: 0px 155px 62px rgba(0, 0, 0, 0.01),
-    0px 87px 52px rgba(0, 0, 0, 0.05), 0px 39px 39px rgba(0, 0, 0, 0.09),
-    0px 10px 21px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);
-
-  border-radius: 23px;
-  transition: all 0.8s cubic-bezier(0.15, 0.83, 0.66, 1);
+  ${backgroundShape}
 `;
 
 const Icon = styled.img`
